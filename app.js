3b8eea6904871e83bd95a373f9313bdce460e08e
Aliabu123 <Aliabualhawa7@gmail.com>
2023-03-29 18:26:26 -0500

    Making progress

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let character = document.getElementById("character");
 let obstacle = document.getElementById('obstacle');
+let obstacle1 = document.getElementById("obstacle1");
+let obstacle2 = document.getElementById("obstacle2");
+let obstacle3 = document.getElementById("obstacle3");
 let gameOver = document.getElementById('game-over')
 let displayScore = document.getElementById('score'); 
 let score = 0;
@@ -30,12 +33,40 @@ var gameValid = setInterval(function(){
 	console.log("ch", characterTop)
 	if(obstacleLeft< 15 && obstacleLeft > 0 && characterTop>=550){
 		obstacle.style.animation = "none";
+		obstacle1.style.animation = "none";
+		obstacle2.style.animation = "none";
+		obstacle3.style.animation = "none";
+		 clearInterval(updateScore)
 		obstacle.style.display = "none";
 		alert("You Lose!! Your To Slow!!")
 		gameOver.classList.add("show")  
 	   } console.log("ch", characterTop)
-      
+
+    //   score++;
+    // showScore();
 }, 20)
+
+// var gameValid = setInterval(function () {
+// 	let characterTop = parseInt(
+// 	  window.getComputedStyle(character).getPropertyValue("top")
+// 	);
+  
+// 	let obstacleLeft2 = parseInt(
+// 	  window.getComputedStyle(obstacle1).getPropertyValue("left")
+// 	);
+  
+// 	console.log("obstacleLeft", obstacleLeft2);
+// 	console.log("ch", characterTop);
+// 	if (obstacleLeft2 < 15 && obstacleLeft2 > 0 && characterTop >= 550) {
+// 	  obstacle.style.animation = "none";
+// 	  obstacle.style.display = "none";
+// 	  alert("You Lose!! Your To Slow!!");
+// 	  gameOver.classList.add("show");
+// 	}
+  
+// 	// score++;
+// 	showScore();
+//   }, 20);
 
 
 
@@ -43,4 +74,6 @@ function showScore(){
 	score ++;
     displayScore.innerText = score;
 }
-setInterval(showScore,200)
+
+ let updateScore = setInterval(showScore,1000)
+
